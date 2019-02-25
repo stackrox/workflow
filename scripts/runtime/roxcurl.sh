4688eaf4db2380186e192f71867c6540eb96d5e9
David Terrell <dbt@stackrox.com>
2019-02-25 14:06:20 -0800

    add support for ${rox_token}

--- scripts/runtime/roxcurl.sh
+++ scripts/runtime/roxcurl.sh
@@ -8,8 +8,6 @@ SCRIPT="$(python -c "import os; print(os.path.realpath('$0'))")"
 source "$(dirname "$SCRIPT")/../../lib/common.sh"
 
 : ${rox_url:=https://localhost:8000}
-: ${rox_dir:=${GOPATH?${HOME}/go}/src/github.com/stackrox/rox}
-password_file="${rox_dir}/deploy/k8s/central-deploy/password"
 
 url="$1"
 shift
@@ -19,8 +17,14 @@ if [[ ! "$url" =~ ^https?:// ]]; then
 fi
 
 auth=()
-if [[ -f "${password_file}" ]]; then
+if [[ -z "${rox_token}" ]]; then
+    : ${rox_dir:=${GOPATH?${HOME}/go}/src/github.com/stackrox/rox}
+    password_file="${rox_dir}/deploy/k8s/central-deploy/password"
+    if [[ -f "${password_file}" ]]; then
 	auth=(-u "admin:$(cat "${password_file}")")
+    fi
+else
+    auth=(-H "Authorization: Bearer ${rox_token}")
 fi
 
 curl -sSk "${auth[@]}" "$url" "$@"
