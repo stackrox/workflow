25c53e27e86ca4c97bf97146610ecd7008e69395
Malte Isberner <misberner@users.noreply.github.com>
2019-05-13 14:34:07 +0200

    improve gogen to accept a target directory (#19)

--- scripts/dev/gogen.sh
+++ scripts/dev/gogen.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-# Runs go generate rooted at the current working directory, but with the PATH expected for mockgen-wrapper to work.
-# Usage: gogen (while inside the rox repo).
+# Runs go generate rooted at the current working directory (or a directory specified as the first
+# argument, if any), but with the PATH expected for mockgen-wrapper to work.
+# Usage: gogen [<directory>] (while inside the rox repo).
 
 SCRIPT="$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${BASH_SOURCE[0]}")"
 source "$(dirname "$SCRIPT")/../../lib/common.sh"
 
 gitroot="$(git rev-parse --show-toplevel)"
 
-PATH="$PATH:${gitroot}/tools/generate-helpers" go generate ./...
+target_dir="${1:-.}"
+
+cd "$target_dir"
+PATH="$PATH:${gitroot}/tools/generate-helpers" go generate "./..."
