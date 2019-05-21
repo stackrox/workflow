ad24df733f3ce11cd03a485cc71af863864f74a1
Viswajith Venugopal <viswajithiii@gmail.com>
2019-05-20 18:03:14 -0700

    Show warning if not in well-known dev context

--- scripts/runtime/teardown.sh
+++ scripts/runtime/teardown.sh
@@ -2,6 +2,21 @@
 
 # Tears down a running StackRox installation very quickly, and makes sure no resources we create are left running around.
 
+SCRIPT="$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${BASH_SOURCE[0]}")"
+source "$(dirname "$SCRIPT")/../../lib/common.sh"
+
+well_known_dev_context_regexes=(docker-for-desktop gke.*setup-dev.*)
+
+current_context="$(kubectl config current-context)"
+
+matched=0
+for regex in "${well_known_dev_context_regexes[@]}"; do
+  if [[ ${current_context} =~ ${regex} ]]; then
+    matched=1
+  fi
+done
+
+(( matched == 1 )) || { yes_no_prompt "Detected that you're connected to a cluster ${current_context}; which is not a typical dev environment. Are you sure you want to proceed with the teardown?"; }
 
 # Delete deployments quickly. If we add a new deployment and forget to add it here, it'll get caught in the next line anyway.
 kubectl -n stackrox delete --grace-period=0 --force deploy/central deploy/sensor ds/collector deploy/monitoring
