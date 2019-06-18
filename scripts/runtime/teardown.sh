e47add85a199bab4b91fb78ef2a2d1b043d44cc3
Viswajith Venugopal <viswajithiii@gmail.com>
2019-06-18 11:29:28 -0700

    Remove application in teardown script

--- scripts/runtime/teardown.sh
+++ scripts/runtime/teardown.sh
@@ -23,7 +23,7 @@ fi
 
 # Delete deployments quickly. If we add a new deployment and forget to add it here, it'll get caught in the next line anyway.
 kubectl -n stackrox delete --grace-period=0 --force deploy/central deploy/sensor ds/collector deploy/monitoring
-kubectl -n stackrox get cm,deploy,ds,networkpolicy,secret,svc,serviceaccount,validatingwebhookconfiguration -o name | xargs kubectl -n stackrox delete --wait
+kubectl -n stackrox get application,cm,deploy,ds,networkpolicy,secret,svc,serviceaccount,validatingwebhookconfiguration -o name | xargs kubectl -n stackrox delete --wait
 
 ## DO NOT RUN THIS IN A CUSTOMER ENVIRONMENT, IT WILL DELETE ALL THEIR DATA
 ## AND THEY WILL NEVER TALK TO US AGAIN.
