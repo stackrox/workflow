a6c01f31fc63eed43068353c455bd7ad156df42f
Viswajith Venugopal <viswajithiii@gmail.com>
2019-08-08 08:20:36 -0700

    Make teardown work even if we didn't define the application

--- scripts/runtime/teardown.sh
+++ scripts/runtime/teardown.sh
@@ -34,7 +34,9 @@ fi
 
 # Delete deployments quickly. If we add a new deployment and forget to add it here, it'll get caught in the next line anyway.
 kubectl -n stackrox delete --grace-period=0 --force deploy/central deploy/sensor ds/collector deploy/monitoring
-kubectl -n stackrox get application,cm,deploy,ds,networkpolicy,secret,svc,serviceaccount,validatingwebhookconfiguration -o name | xargs kubectl -n stackrox delete --wait
+kubectl -n stackrox get application -o name | xargs kubectl -n stackrox delete --wait
+#
+kubectl -n stackrox get cm,deploy,ds,networkpolicy,secret,svc,serviceaccount,validatingwebhookconfiguration -o name | xargs kubectl -n stackrox delete --wait
 # Only delete RBAC/PSP-related resources that contain "stackrox"
 kubectl -n stackrox get clusterrole,clusterrolebinding,role,rolebinding,psp -o name | grep stackrox | xargs kubectl -n stackrox delete --wait
 
