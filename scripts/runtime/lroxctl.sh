a97ac5a5abb0870bb6506939883e90fa44f9a4cb
Connor Gorman <cgorman@stackrox.com>
2020-06-08 12:16:59 -0700

    gpp -> getpwd

--- scripts/runtime/lroxctl.sh
+++ scripts/runtime/lroxctl.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [[ -z "${ROX_API_TOKEN}" ]]; then
-  roxctl --insecure-skip-tls-verify -e localhost:8000 -p $(gpp) $@
+  roxctl --insecure-skip-tls-verify -e localhost:8000 -p $(getpwd) $@
 else
   roxctl --insecure-skip-tls-verify -e localhost:8000  $@
 fi
