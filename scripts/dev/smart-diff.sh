860844a681b26ad0e5f1f18dde36525a43f9d6d9
Moritz Clasmeier <moritz@stackrox.com>
2020-12-02 23:28:34 +0100

    Added comment

--- scripts/dev/smart-diff.sh
+++ scripts/dev/smart-diff.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Produces git diff relative to the last smart-branch commit.
+
 SCRIPT="$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${BASH_SOURCE[0]}")"
 source "$(dirname "$SCRIPT")/../../lib/git.sh"
 
