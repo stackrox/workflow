21a8564f6ec71ea6833d34e8d30800166b3ca64a
Simon Baeumer <simon@stackrox.com>
2020-12-07 10:35:44 +0100

    Add pip dependencies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
 SCRIPT="$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${BASH_SOURCE[0]}")"
 source "$(dirname "$SCRIPT")/lib/common.sh"
+
+python_interpreter="$(which python3 || which python)"
+einfo "Check pip installation, using ${python_interpreter}"
+if ! "${python_interpreter}" -m pip --version > /dev/null; then
+  einfo "Pip not found, installing pip"
+  einfo "Root access required for installing pip"
+  sudo -H "${python_interpreter}" -m ensurepip
+fi
+
+einfo "Installing python packages"
+einfo "Root access is required to install pip requirements"
+"${python_interpreter}" -m pip install -r requirements.txt
 
 platform="$(uname)"
 
@@ -21,6 +34,9 @@ if [[ "$platform" == "Linux" ]]; then
     fi
   fi
 fi
+
+einfo "Installing python packages"
+python -m pip install requests
 
 [[ -n "$setup_script" ]] || die "Unsupported platform: $platform"
 
