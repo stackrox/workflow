10393ee7cf139013e1a5a5df522d817ccc769ee4
Mikhail Sugakov <misha@stackrox.com>
2021-02-05 11:12:43 +0100

    Address MacOS Docker privelege issue in `roxdebug` script
    so that users don't need to worry about doing it manually.
    
    See the similar fix made in https://github.com/stackrox/rox/pull/7553
    for a bit more context.

--- scripts/dev/roxdebug.sh
+++ scripts/dev/roxdebug.sh
@@ -28,15 +28,18 @@ positional=()
 while (("$#")); do
   case "$1" in
   -h | --help)
-    usage ;;
+    usage
+    ;;
   -p | --port)
     if [[ "${2:-undefined}" == "undefined" ]]; then
       usage "Missing value for $1 argument."
     fi
     port=${2}
-    shift ;;
+    shift
+    ;;
   *)
-    positional+=("$1") ;;
+    positional+=("$1")
+    ;;
   esac
   shift
 done
@@ -47,6 +50,42 @@ fi
 
 deployment=${positional[0]:-$DEFAULT_DEPLOYMENT}
 port=${port:-$DEFAULT_PORT}
+
+function ensure_debugging_is_allowed() {
+  ptrace_scope_file="/proc/sys/kernel/yama/ptrace_scope"
+  # If /proc/sys/kernel/yama/ptrace_scope is present and it contains anything non-zero, the following error will appear
+  # during `dlv attach` attempt:
+  #    Could not attach to pid 1: this could be caused by a kernel security setting, try writing "0" to /proc/sys/kernel/yama/ptrace_scope
+  # This is likely on MacOS Docker and in some cloud environments.
+
+  # First, we check if anything needs adjustment. We read the current value by exec-ing into the existing deployment
+  # because a) that's much faster than spinning up a new image, and b) we read the value from the same node where
+  # the deployment is running.
+  ptrace_scope_state=$(kubectl --namespace stackrox exec "${deployment}" -- /bin/sh -c "[ -e ${ptrace_scope_file} ] && cat ${ptrace_scope_file} || echo 0")
+  if [[ "${ptrace_scope_state}" != "0" ]]; then
+    echo "${ptrace_scope_file} is currently set to ${ptrace_scope_state}, trying to change it to 0"
+
+    # Find out node where the pod is deployed.
+    if [[ "${deployment}" =~ ^deploy/ ]]; then # $deployment is like "deploy/central"
+      # We rely on `app` label (e.g. app=central).
+      # In case this turns unreliable, something like this should be done https://github.com/kubernetes/kubernetes/issues/72794#issuecomment-483502617
+      selector="--selector=app=${deployment/deploy\//}"
+      jsonpath="{.items[0].spec.nodeName}"
+    else # $deployment it is just a pod name e.g. "central-d5bffdf6-wlx5c"
+      selector="${deployment}"
+      jsonpath="{.spec.nodeName}"
+    fi
+    node_name=$(kubectl -n stackrox get pods "${selector}" --output jsonpath="${jsonpath}")
+
+    # Run one-off privileged container on the target node. The container will write 0 to the file.
+    # The file is mounted from the node and so the privileged container should be able to change it for all pods on the node.
+    kubectl -n stackrox run -it --rm --restart=Never \
+      --privileged \
+      --overrides "{ \"spec\": { \"nodeName\": \"${node_name}\" } }" \
+      --image=alpine debug-enabler -- /bin/sh -c "echo 0 > ${ptrace_scope_file}"
+  fi
+}
+ensure_debugging_is_allowed
 
 echo "Starting port forwarding and debugger for '${deployment}' on port '${port}'. Hit Control-C or Control-\ to stop..."
 
