30a153ba7fbe14d0027c804cc7a9a2696bc6a9d2
gavin-stackrox <gavin@stackrox.com>
2021-05-19 10:59:45 -0700

    refactor

--- tools/roxci/check.go
+++ tools/roxci/check.go
@@ -12,6 +12,20 @@ import (
 	"strings"
 )
 
+const (
+	COMMIT_ARG_RECIPE  = "recipe"
+	COMMIT_ARG_EXCLUDE = "exclude"
+	COMMIT_ARG_INCLUDE = "include"
+)
+
+var (
+	COMMIT_ARGS = map[string]bool{
+		COMMIT_ARG_RECIPE:  true,
+		COMMIT_ARG_EXCLUDE: true,
+		COMMIT_ARG_INCLUDE: true,
+	}
+)
+
 func CheckCommand() *cobra.Command {
 	// $ roxci check <job name>
 	cmd := &cobra.Command{
@@ -21,56 +35,113 @@ func CheckCommand() *cobra.Command {
 The exit code can be used to decide whether or not to run a job:
   0 => skip this job
   1 => run
-any other exit code implies that an error occurred but the job should still be run.`,
+Any other exit code implies that an error occurred but the job should still be run.`,
 		Args: cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			log.SetLevel(log.Info)
-
-			git := exec.Command("git", "cherry", "-v", "master")
-
-			var outb bytes.Buffer
-			git.Stdout = &outb
-			git.Stderr = &outb
-
-			err := git.Run()
-			if err != nil {
-				log.Errorf("Cannot list commits: %v", err)
-				fmt.Print(outb.String())
-				os.Exit(2)
-			}
-			commits := strings.Split(outb.String(), "\n")
-			for i, j := 0, len(commits)-1; i < j; i, j = i+1, j-1 {
-				commits[i], commits[j] = commits[j], commits[i]
-			}
-
-			configFile, ok := os.LookupEnv("ROXCI_CONFIG_FILE")
-			if !ok {
-				configFile = ".circleci/roxci.yml"
-			}
-			recipes, err := LoadRecipes(configFile)
-			if err != nil {
-				log.Errorf("Cannot load recipes: %v", err)
-				os.Exit(2)
-			}
-
-			run, err := Check(args[0], commits, recipes)
-			if err != nil {
-				log.Errorf("Check failed: %v", err)
-				os.Exit(2)
-			}
-
-			if run {
-				os.Exit(1)
-			}
-
-			os.Exit(0)
-		},
+		Run:  checkCommand,
 	}
 
 	return cmd
 }
 
-func Check(job string, commitMessages []string, recipes *[]Recipe) (bool, error) {
+func checkCommand(cmd *cobra.Command, args []string) {
+	log.SetLevel(log.Info)
+
+	git := exec.Command("git", "cherry", "-v", "master")
+
+	var outb bytes.Buffer
+	git.Stdout = &outb
+	git.Stderr = &outb
+
+	err := git.Run()
+	if err != nil {
+		log.Errorf("Cannot list commits: %v", err)
+		fmt.Print(outb.String())
+		os.Exit(2)
+	}
+	commits := strings.Split(outb.String(), "\n")
+	for i, j := 0, len(commits)-1; i < j; i, j = i+1, j-1 {
+		commits[i], commits[j] = commits[j], commits[i]
+	}
+
+	configFile, ok := os.LookupEnv("ROXCI_CONFIG_FILE")
+	if !ok {
+		configFile = ".circleci/roxci.yml"
+	}
+	recipes, err := LoadRecipes(configFile)
+	if err != nil {
+		log.Errorf("Cannot load recipes: %v", err)
+		os.Exit(2)
+	}
+
+	run, err := check(args[0], commits, recipes)
+	if err != nil {
+		log.Errorf("Check failed: %v", err)
+		os.Exit(2)
+	}
+
+	if run {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func check(job string, commitMessages []string, recipes *[]Recipe) (bool, error) {
+	roxciCommandStr := getCommandFromCommits(commitMessages)
+	if roxciCommandStr == "" {
+		return true, nil
+	}
+
+	commitArgs, err := getCommitArgsFromCommand(roxciCommandStr)
+	if err != nil {
+		return true, err
+	}
+
+	for _, excluded := range commitArgs[COMMIT_ARG_EXCLUDE] {
+		if job == excluded {
+			log.Infof("Job '%s' is explicitly excluded", job)
+			return false, nil
+		}
+	}
+	for _, included := range commitArgs[COMMIT_ARG_INCLUDE] {
+		if job == included {
+			log.Infof("Job '%s' is explicitly included", job)
+			return true, nil
+		}
+	}
+	return checkRecipesForJob(job, commitArgs[COMMIT_ARG_RECIPE], recipes)
+}
+
+func checkRecipesForJob(checkJob string, checkRecipes []string, recipes *[]Recipe) (bool, error) {
+	for _, checkRecipe := range checkRecipes {
+		if checkRecipe == "default" {
+			return true, nil
+		}
+
+		log.Infof("Checking for job '%s' in recipe '%s'", checkJob, checkRecipe)
+
+		recipeFound := false
+		for _, recipe := range *recipes {
+			if recipe.Name == checkRecipe {
+				recipeFound = true
+				for job, _ := range recipe.Jobs {
+					if job == checkJob {
+						log.Infof("Job '%s' is included", checkJob)
+						return true, nil
+					}
+				}
+				log.Infof("Job '%s' is not included in recipe '%s'", checkJob, checkRecipe)
+			}
+		}
+		if !recipeFound {
+			return true, errors.New("there is no such recipe: " + checkRecipe)
+		}
+	}
+	log.Infof("Job '%s' is not included in any recipes", checkJob)
+	return false, nil
+}
+
+func getCommandFromCommits(commitMessages []string) string {
 	roxciCommandRe := regexp.MustCompile(`(?i)\s*/roxci (.*)`)
 	var roxciCommandStr string
 	for _, message := range commitMessages {
@@ -80,85 +151,39 @@ func Check(job string, commitMessages []string, recipes *[]Recipe) (bool, error)
 			break
 		}
 	}
-	if roxciCommandStr == "" {
-		return true, nil
-	}
-	roxciCommandStr = strings.ToLower(roxciCommandStr)
-
-	var runRecipes []string
-	var exclude []string
-	var include []string
-	pieces := strings.Split(roxciCommandStr, " ")
-	if len(pieces) >= 1 {
-		for idx, argString := range pieces {
-			argPieces := strings.Split(argString, "=")
-			if idx == 0 && len(argPieces) == 1 {
-				runRecipes = append(runRecipes, strings.Split(argString, ",")...)
-				continue
-			}
-			if len(argPieces) != 2 {
-				return true, errors.Errorf(
-					"%s is an unexpected /roxci arg",
-					argString,
-				)
-			}
-			switch argPieces[0] {
-			case "recipe":
-				runRecipes = append(runRecipes, strings.Split(argPieces[1], ",")...)
-			case "exclude":
-				exclude = append(exclude, strings.Split(argPieces[1], ",")...)
-			case "include":
-				include = append(include, strings.Split(argPieces[1], ",")...)
-			default:
-				return true, errors.Errorf(
-					"%s is an unexpected /roxci arg",
-					argString,
-				)
-			}
-		}
-		for _, excluded := range exclude {
-			if job == excluded {
-				log.Infof("Job '%s' is explicitly excluded", job)
-				return false, nil
-			}
-		}
-		for _, included := range include {
-			if job == included {
-				log.Infof("Job '%s' is explicitly included", job)
-				return true, nil
-			}
-		}
-		return checkRunRecipeForJob(job, runRecipes, recipes)
-	}
-
-	return true, nil
+	return strings.ToLower(roxciCommandStr)
 }
 
-func checkRunRecipeForJob(runJob string, runRecipes []string, recipes *[]Recipe) (bool, error) {
-	for _, runRecipe := range runRecipes {
-		if runRecipe == "default" {
-			return true, nil
-		}
+func getCommitArgsFromCommand(command string) (map[string][]string, error) {
 
-		log.Infof("Checking for job '%s' in recipe '%s'", runJob, runRecipe)
-
-		recipeFound := false
-		for _, recipe := range *recipes {
-			if recipe.Name == runRecipe {
-				recipeFound = true
-				for job, _ := range recipe.Jobs {
-					if job == runJob {
-						log.Infof("Job '%s' is included", runJob)
-						return true, nil
-					}
-				}
-				log.Infof("Job '%s' is not included in recipe '%s'", runJob, runRecipe)
-			}
-		}
-		if !recipeFound {
-			return true, errors.New("there is no such recipe: " + runRecipe)
-		}
+	var commitArgs = map[string][]string{
+		COMMIT_ARG_RECIPE:  {},
+		COMMIT_ARG_EXCLUDE: {},
+		COMMIT_ARG_INCLUDE: {},
 	}
-	log.Infof("Job '%s' is not included in any recipes", runJob)
-	return false, nil
+
+	pieces := strings.Split(command, " ")
+
+	for idx, argString := range pieces {
+		argPieces := strings.Split(argString, "=")
+		if idx == 0 && len(argPieces) == 1 {
+			commitArgs[COMMIT_ARG_RECIPE] = append(commitArgs[COMMIT_ARG_RECIPE], strings.Split(argString, ",")...)
+			continue
+		}
+		if len(argPieces) != 2 {
+			return nil, errors.Errorf(
+				"%s is an unexpected /roxci arg",
+				argString,
+			)
+		}
+		if ok := COMMIT_ARGS[argPieces[0]]; !ok {
+			return nil, errors.Errorf(
+				"%s is an unexpected /roxci arg",
+				argString,
+			)
+		}
+		commitArgs[argPieces[0]] = append(commitArgs[argPieces[0]], strings.Split(argPieces[1], ",")...)
+	}
+
+	return commitArgs, nil
 }
--- tools/roxci/check_test.go
+++ tools/roxci/check_test.go
@@ -10,147 +10,147 @@ var (
 )
 
 func TestNoCommits(t *testing.T) {
-	run, err := Check("a", []string{}, validRecipes)
+	run, err := check("a", []string{}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobInSimpleRecipeCommand(t *testing.T) {
-	run, err := Check("a", []string{"/roxci one"}, validRecipes)
+	run, err := check("a", []string{"/roxci one"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobInVerboseRecipeCommand(t *testing.T) {
-	run, err := Check("a", []string{"/roxci recipe=two"}, validRecipes)
+	run, err := check("a", []string{"/roxci recipe=two"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobNotInSimpleRecipeCommand(t *testing.T) {
-	run, err := Check("d", []string{"/roxci one"}, validRecipes)
+	run, err := check("d", []string{"/roxci one"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestJobNotInVerboseRecipeCommand(t *testing.T) {
-	run, err := Check("d", []string{"/roxci recipe=two"}, validRecipes)
+	run, err := check("d", []string{"/roxci recipe=two"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestMissingRecipe(t *testing.T) {
-	run, err := Check("a", []string{"/roxci four"}, validRecipes)
+	run, err := check("a", []string{"/roxci four"}, validRecipes)
 	assert.EqualError(t, err, "there is no such recipe: four")
 	assert.True(t, run)
 }
 
 func TestMissingVerboseRecipe(t *testing.T) {
-	run, err := Check("a", []string{"/roxci recipe=four"}, validRecipes)
+	run, err := check("a", []string{"/roxci recipe=four"}, validRecipes)
 	assert.EqualError(t, err, "there is no such recipe: four")
 	assert.True(t, run)
 }
 
 func TestJobExcluded(t *testing.T) {
-	run, err := Check("a", []string{"/roxci recipe=one exclude=a"}, validRecipes)
+	run, err := check("a", []string{"/roxci recipe=one exclude=a"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestJobExcludedWhenMany(t *testing.T) {
-	run, err := Check("a", []string{"/roxci recipe=one exclude=b,a"}, validRecipes)
+	run, err := check("a", []string{"/roxci recipe=one exclude=b,a"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestJobInComposedRecipes(t *testing.T) {
-	run, err := Check("c", []string{"/roxci one,two"}, validRecipes)
+	run, err := check("c", []string{"/roxci one,two"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
-	run, err = Check("a", []string{"/roxci one,two"}, validRecipes)
+	run, err = check("a", []string{"/roxci one,two"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobNotInComposedRecipes(t *testing.T) {
-	run, err := Check("d", []string{"/roxci one,two"}, validRecipes)
+	run, err := check("d", []string{"/roxci one,two"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestJobInComposedRecipesII(t *testing.T) {
-	run, err := Check("c", []string{"/roxci recipe=one,two"}, validRecipes)
+	run, err := check("c", []string{"/roxci recipe=one,two"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
-	run, err = Check("a", []string{"/roxci recipe=two recipe=one"}, validRecipes)
+	run, err = check("a", []string{"/roxci recipe=two recipe=one"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobExcludedWhenManyMore(t *testing.T) {
-	run, err := Check("a", []string{"/roxci recipe=one exclude=b exclude=a"}, validRecipes)
+	run, err := check("a", []string{"/roxci recipe=one exclude=b exclude=a"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestCaseAmbivalence(t *testing.T) {
-	run, err := Check("a", []string{"/roXci recIpe=oNe eXclude=b eXclude=a"}, validRecipes)
+	run, err := check("a", []string{"/roXci recIpe=oNe eXclude=b eXclude=a"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestCaseAmbivalenceII(t *testing.T) {
-	run, err := Check("a", []string{"/roXci recIpe=oNe eXclude=b eXclude=A"}, validRecipes)
+	run, err := check("a", []string{"/roXci recIpe=oNe eXclude=b eXclude=A"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestCaseAmbivalenceIII(t *testing.T) {
-	run, err := Check("a", []string{"/roXci recIpe=oNe"}, validRecipes)
+	run, err := check("a", []string{"/roXci recIpe=oNe"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobIncluded(t *testing.T) {
-	run, err := Check("d", []string{"/roxci recipe=one include=d"}, validRecipes)
+	run, err := check("d", []string{"/roxci recipe=one include=d"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobIncludedMany(t *testing.T) {
-	run, err := Check("e", []string{"/roxci recipe=one include=e,d"}, validRecipes)
+	run, err := check("e", []string{"/roxci recipe=one include=e,d"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestJobIncludedManyMore(t *testing.T) {
-	run, err := Check("e", []string{"/roxci recipe=one include=e include=d"}, validRecipes)
+	run, err := check("e", []string{"/roxci recipe=one include=e include=d"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestManyCommits(t *testing.T) {
-	run, err := Check("e", []string{"testing", "/roxci recipe=one include=e,d", "123"}, validRecipes)
+	run, err := check("e", []string{"testing", "/roxci recipe=one include=e,d", "123"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestManyCommitsMostRecentWins(t *testing.T) {
-	run, err := Check("e",
+	run, err := check("e",
 		[]string{"testing", "/roxci recipe=one", "/roxci recipe=one include=e,d", "123"}, validRecipes)
 	assert.Nil(t, err)
 	assert.False(t, run)
 }
 
 func TestExplicitDefault(t *testing.T) {
-	run, err := Check("e",
+	run, err := check("e",
 		[]string{"testing", "/roxci default", "/roxci recipe=one include=e,d", "123"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
 }
 
 func TestMoreExplicitDefault(t *testing.T) {
-	run, err := Check("e",
+	run, err := check("e",
 		[]string{"testing", "/roxci recipe=default", "/roxci recipe=one include=e,d", "123"}, validRecipes)
 	assert.Nil(t, err)
 	assert.True(t, run)
