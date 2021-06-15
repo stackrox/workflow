9a3b0155f531ca1a5180dec76dda13f9b0a1726c
Viswajith Venugopal <viswajithiii@gmail.com>
2021-06-15 11:21:34 -0700

    Remove outdated code in roxkubectx which queried setup.rox.systems (#72)

--- scripts/runtime/roxkubectx.sh
+++ scripts/runtime/roxkubectx.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
 
-# A `kubectl config current-context` wrapper that is aware of setup names.
+# [DEPRECATED] Alias for `kubectl config current-context`.
+# This command is deprecated and will eventually be removed.
+# Please just use `kubectl config current-context` (or your own alias for it) instead.
+
 
 SCRIPT="$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${BASH_SOURCE[0]}")"
-source "$(dirname "$SCRIPT")/../../lib/common.sh"
 
-curr_context="$(kubectl config current-context)"
-[[ -n "${curr_context}" ]] || die "Couldn't determine current context"
-
-workfile_for_context="$(get_workfile_path_and_ensure_dir kubectx)"
-[[ -n "${workfile_for_context}" ]] || die "Coudn't get workfile"
-
-if [[ -f "${workfile_for_context}" ]]; then
-    cached_context="$(jq --arg context "${curr_context}" '.[$context] // empty' -r <"${workfile_for_context}")"
-fi
-if [[ -n "${cached_context}" ]]; then
-    echo "${cached_context}"
-    exit 0
-fi
-
-[[ -n "{PYTHON_INTERPRETER}" ]] || die "No Python interpreter found"
-"${PYTHON_INTERPRETER}" "$(dirname "$SCRIPT")/setup_id_to_name.py" "${curr_context}" "${workfile_for_context}"
+kubectl config current-context
