7d54094d93a7ab86e7c12a70f08868a356e6b70d
Nakul Chander <nakul@stackrox.com>
2021-10-28 11:21:14 -0700

    review comments

--- scripts/dev/mergekconf.sh
+++ scripts/dev/mergekconf.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+# This command merges the provided kubeconfig file into the base one used by kubectl.
+# Usage: mergekconf [-k base kubeconfig file] [-b] [-r new context and user name] [file to merge]
+#   [file to merge]  Path to new kubeconfig file to merge
+#   -k path          Location of base kubeconfig file. This will be overriden.
+#                    Default location: value of $KUBECONFIG if it's set or $HOME/.kube/config.
+#   -b               Create backup of the base file before merge.
+#   -r name          Rename context _and_ user to chosen name. NOTE: ONLY WORKS IF CONFIG HAS EXACTLY ONE CONTEXT & USER
+#   -h               This message.
+
+
 set -euo pipefail
 set +x
 
-DEFAULT_BASE_KUBECONFIG="$HOME/.kube/config"
+DEFAULT_BASE_KUBECONFIG=${KUBECONFIG:-"$HOME/.kube/config"}
 
 function usage() {
-  echo "Usage: $(basename "$0") [-k base kubeconfig file] [-b] [-r new context and user name] [file to merge] "
-  echo "  [file to merge]  Path to new kubeconfig file to merge"
-  echo "  -k path          Location of base kubeconfig file. This will be overriden."
-  echo "                   Default location: $DEFAULT_BASE_KUBECONFIG."
-  echo "  -b               Create backup of the base file before merge."
-  echo "  -r name          Rename context _and_ user to chosen name. NOTE: ONLY WORKS IF CONFIG HAS EXACTLY ONE CONTEXT & USER"
-  echo "  -h               This message."
-  echo ""
-  echo "This command merges the provided kubeconfig file into the base one used by kubectl."
+  roxhelp "$(basename "$0")"
   if [[ -n "${1:-}" ]]; then
     echo ""
     echo >&2 "Error: $1"
@@ -36,14 +38,14 @@ while (("$#")); do
     create_backup=true
     ;;
   -r)
-    if [[ "${2:-undefined}" == "undefined" ]]; then
+    if [[ "${2:-}" == "" ]]; then
       usage "Missing value for $1 argument."
     fi
     rename=${2}
     shift
     ;;
   -k)
-    if [[ "${2:-undefined}" == "undefined" ]]; then
+    if [[ "${2:-}" == "" ]]; then
       usage "Missing value for $1 argument."
     fi
     base_kubeconfig=${2}
@@ -66,19 +68,19 @@ base_kubeconfig=${base_kubeconfig:-$DEFAULT_BASE_KUBECONFIG}
 # Create a backup if necessary
 if ($create_backup); then
 	backup_file=$(mktemp "/tmp/kconfig.bak.XXXXX")
-	cp "${base_kubeconfig[@]}" ${backup_file}
+	cp -p "${base_kubeconfig[@]}" ${backup_file}
 	echo "Find backup in ${backup_file}"
 fi
 
 # Rename context and user if necessary
-if [[ "${rename:-undefined}" != "undefined" ]]; then
+if [[ "${rename:-}" != "" ]]; then
 	# Copy the kube file to merge over so that the orig file isn't touched
 	tmp_renamed=$(mktemp "/tmp/kconfig.renamed.XXXXX")
-	cp "${file}" ${tmp_renamed}
+	cp -p "${file}" ${tmp_renamed}
 
 	# Get the existing context and username
 	# TODO: Make this more elegant if there are more than 1. Maybe error out?
-	contextname=$(KUBECONFIG=${tmp_renamed} kubectl config view -ojsonpath='{.contexts[0].name}')
+	contextname=$(KUBECONFIG=${tmp_renamed} kubectl config current-context)
 	username=$(KUBECONFIG=${tmp_renamed} kubectl config view -ojsonpath='{.users[0].name}')
 
 	KUBECONFIG=${tmp_renamed} kubectl config rename-context "${contextname}" "${rename}" > /dev/null
