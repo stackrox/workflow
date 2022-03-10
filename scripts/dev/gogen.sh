85a205c855c582a033d898387bbf0002cc96f702
Viswajith Venugopal <viswajithiii@gmail.com>
2022-03-10 08:01:59 -0800

    Make gogen support -run (#92)

--- scripts/dev/gogen.sh
+++ scripts/dev/gogen.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # Runs go generate rooted at the current working directory (or a directory specified as the first
-# argument, if any), but with the PATH expected for mockgen-wrapper to work.
-# Usage: gogen [<directory>] (while inside the rox repo).
+# argument, if any), but with the PATH expected for our custom binaries to work.
+# You can also pass a -run parameter if you want, which will be passed through to go generate to filter
+# which commands are run.
+# Usage: gogen [-run "pattern"] [<directory>] (while inside the stackrox repo).
 
 SCRIPT="$(python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${BASH_SOURCE[0]}")"
 source "$(dirname "$SCRIPT")/../../lib/common.sh"
@@ -15,11 +17,33 @@ fi
 
 export PATH="$PATH:${gitroot}/tools/generate-helpers"
 
+positional_args=()
+run=()
+
+while [[ $# -gt 0 ]]; do
+  arg="$1"
+  shift
+  case "$arg" in
+    -run)
+      val="$1"
+      shift
+      run=(-run "$val")
+      einfo "Will pass ${run[@]} to go generate"
+      ;;
+    *)
+      positional_args+=("$arg") # save positional arg
+      ;;
+  esac
+done
+
 private_gogen() {
   target_dir=$1
-  echo "Generating for ${target_dir}"
-  ( cd "$target_dir" && go generate "./..." )
+  einfo "Generating for ${target_dir}"
+  ( cd "$target_dir" && go generate "${run[@]}" "./..." )
 }
+
+
+set -- "${positional_args[@]}" # restore positional parameters
 
 if [[ "$#" -eq 0 ]]; then
   # Default argument is current working directory.
