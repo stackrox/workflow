f17ce5afe3c879586c633deff37d2269bca61197
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2025-12-03 15:27:53 +0100

    Add more things to cleanup to the teardown script
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- scripts/runtime/teardown.sh
+++ scripts/runtime/teardown.sh
@@ -7,6 +7,14 @@ source "$(dirname "$SCRIPT")/../../lib/common.sh"
 
 test_in_well_known_dev_context
 
+# Uninstall Helm releases first to let Helm clean up its managed resources properly.
+for release in stackrox-central-services stackrox-secured-cluster-services stackrox-monitoring; do
+  if helm status "$release" -n stackrox &>/dev/null; then
+    einfo "Uninstalling Helm release: $release"
+    helm uninstall "$release" -n stackrox --wait || true
+  fi
+done
+
 # Collect all stackrox PVs before we delete the respective PVCs.
 IFS=$'\n' read -d '' -r -a stackrox_pvs < <(
   kubectl get pv -o json | jq -r '.items[] | select(.spec.claimRef.namespace == "stackrox") | .metadata.name'
@@ -20,6 +28,15 @@ kubectl -n stackrox get application -o name | xargs kubectl -n stackrox delete -
 kubectl -n stackrox get cm,deploy,ds,hpa,networkpolicy,role,rolebinding,secret,svc,serviceaccount,pvc -o name | xargs kubectl -n stackrox delete --wait
 # Only delete cluster-wide RBAC/PSP-related resources that contain have the app.kubernetes.io/name=stackrox label.
 kubectl -n stackrox get clusterrole,clusterrolebinding,psp,validatingwebhookconfiguration -o name -l app.kubernetes.io/name=stackrox | xargs kubectl -n stackrox delete --wait
+
+# Delete the SecurityPolicy CRD which is not labeled and can cause "managedFields must be nil" errors
+# if left behind when reinstalling via Helm.
+kubectl delete crd securitypolicies.config.stackrox.io --ignore-not-found
+
+# Delete monitoring ClusterRoles/ClusterRoleBindings that are managed by the stackrox-monitoring Helm release
+# and may not have the app.kubernetes.io/name=stackrox label.
+kubectl delete clusterrole stackrox-monitoring stackrox-monitoring-kube-state-metrics --ignore-not-found
+kubectl delete clusterrolebinding stackrox-monitoring-kube-state-metrics --ignore-not-found
 
 ## DO NOT RUN THIS IN A CUSTOMER ENVIRONMENT, IT WILL DELETE ALL THEIR DATA
 ## AND THEY WILL NEVER TALK TO US AGAIN.
